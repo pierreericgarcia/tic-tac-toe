b2ecdf0536795c8821ea9a3f77349ffd0071f22c
Pierre-Eric Garcia <garciapierreeric@gmail.com>
2021-11-10 15:01:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+// useState: tic tac toe
+// http://localhost:3000/isolated/exercise/04.js
 
-function App() {
+import * as React from "react";
+
+export const emptySquares = Array(9).fill(null);
+
+export const calculateNextValue = (squares) => {
+  const xSquares = squares.filter((s) => s === "X");
+  const oSquares = squares.filter((s) => s === "O");
+  if (xSquares.length === oSquares.length) {
+    return "X";
+  }
+  return xSquares.length > oSquares.length ? "O" : "X";
+};
+
+export const calculateWinner = (squares) => {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkLine = (line, squares) => {
+    const correspondingSquares = squares.filter((s, i) => line.includes(i));
+    const isCorrespondingSquaresMatching = correspondingSquares.every(
+      (s) => s === correspondingSquares[0]
+    );
+    return isCorrespondingSquaresMatching ? correspondingSquares[0] : null;
+  };
+
+  return winningLines.reduce(
+    (winner, line) =>
+      checkLine(line, squares) ? checkLine(line, squares) : winner,
+    null
+  );
+};
+
+export const selectSquare = (squareIndex, squares) => {
+  const nextValue = calculateNextValue(squares);
+  const result = squares.map((s, i) => (i === squareIndex ? nextValue : s));
+  return result;
+};
+
+
+export const Board = () => {
+  const [squares, setQuares] = React.useState(emptySquares);
+  const winner = calculateWinner(squares);
+
+  const play = (squareIndex) => {
+    setQuares(selectSquare(squareIndex, squares));
+  };
+
+  const restart = () => {
+    setQuares(emptySquares);
+  };
+
+  const calculateStatus = (winner, nextValue) => {
+    if (winner) {
+      return `The winner is ${winner} !`;
+    }
+
+    return `${nextValue} it's your turn !`;
+  };
+
+  function renderSquare(i) {
+    return (
+      <button
+        disabled={squares[i] || winner}
+        className="square"
+        onClick={() => play(i)}
+      >
+        {squares[i]}
+      </button>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="status">STATUS : {calculateStatus(winner, calculateNextValue(squares))}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button className="restart" onClick={restart}>
+        restart
+      </button>
     </div>
   );
+};
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return <Game />;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { calculateNextValue, calculateWinner, emptySquares, selectSquare } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Frist Moove : Player 1 selects the fourth square", () => {
+  expect(selectSquare(3, emptySquares)).toStrictEqual([
+    null,
+    null,
+    null,
+    "X",
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+});
+
+test("Second Moove : Player 2 selects the first square", () => {
+  expect(
+    selectSquare(0, [null, null, null, "X", null, null, null, null, null])
+  ).toStrictEqual(["O", null, null, "X", null, null, null, null, null]);
+});
+
+test("Which one is the next to play ? fifth tour.", () => {
+  expect(
+    calculateNextValue(["O", null, null, "X", "X", null, "O", null, null])
+  ).toStrictEqual("X");
+});
+
+test("Which one is the next to play ? sixth tour.", () => {
+  expect(
+    calculateNextValue(["O", null, 'X', "X", "X", null, "O", null, null])
+  ).toStrictEqual("O");
+});
+
+
+test("Which one is the winner ?", () => {
+  expect(
+    calculateWinner(["O", 'O', 'O', "X", "X", null, "X", null, 'X'])
+  ).toStrictEqual("O");
 });
